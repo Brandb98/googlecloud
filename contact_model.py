9e0a8f054e29fac69c33e7764304c250dd6f4032
Brandb98 <brahyanbedpya@hotmail.es>
2020-07-12 16:00:50 -0500

    dataStore
    Solución 1 en base al comentario

--- contact_model.py
+++ contact_model.py
@@ -1,4 +1,4 @@
-from google.appengine.ext import ndb
+from google.cloud import ndb
 
 class Contact(ndb.Model):
     name = ndb.StringProperty()
--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request
+from google.cloud import ndb
 from contact_model import Contact
-app = Flask(__name__)
 
+app = Flask(__name__)
+client = ndb.Client(project="hiwold")
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
     return render_template('contact_book.html')
 
-
 @app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
 
     if request.form:
-        contact = Contact(name=request.form.get('name'),
-                          phone=request.form.get('phone'),
-                          email=request.form.get('email'))
-
+        with client.context():
+            contact = Contact(
+                            name=request.form.get('name'),
+                            phone=request.form.get('phone'),
+                            email=request.form.get('email')
+                        )
         contact.put()
 
     return render_template('add_contact.html')
-
-
 
 if __name__ == '__main__':
     app.run()
