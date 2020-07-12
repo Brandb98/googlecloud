1a8f49243980c28c93f1e47a4e1bfb87822c5775
Brandb98 <brahyanbedpya@hotmail.es>
2020-07-12 16:26:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-from flask import Flask, render_template, request
 from google.cloud import ndb
-from contact_model import Contact
+client = ndb.Client()
+client
 
-app = Flask(__name__)
-client = ndb.Client(project="hiwold")
+# from flask import Flask, render_template, request
+# from google.cloud import ndb
+# from contact_model import Contact
 
-@app.route(r'/', methods=['GET'])
-def contact_book():
-    return render_template('contact_book.html')
+# app = Flask(__name__)
+# client = ndb.Client(project="hiwold")
 
-@app.route(r'/add', methods=['GET', 'POST'])
-def add_contact():
+# @app.route(r'/', methods=['GET'])
+# def contact_book():
+#     return render_template('contact_book.html')
 
-    if request.form:
-        with client.context():
-            contact = Contact(
-                            name=request.form.get('name'),
-                            phone=request.form.get('phone'),
-                            email=request.form.get('email')
-                        )
-        contact.put()
+# @app.route(r'/add', methods=['GET', 'POST'])
+# def add_contact():
 
-    return render_template('add_contact.html')
+#     if request.form:
+#         with client.context():
+#             contact = Contact(
+#                             name=request.form.get('name'),
+#                             phone=request.form.get('phone'),
+#                             email=request.form.get('email')
+#                         )
+#         contact.put()
 
-if __name__ == '__main__':
-    app.run()
+#     return render_template('add_contact.html')
+
+# if __name__ == '__main__':
+#     app.run()
